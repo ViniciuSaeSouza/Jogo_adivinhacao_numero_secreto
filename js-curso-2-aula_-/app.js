f5467e7778c4416ea60691baff92856f37833558
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-22 20:16:32 -0300

    download base project for exercise 01

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,21 @@
+let title = document.querySelector("h1");
+
+title.innerHTML = "Jogo do número secreto";
+
+
+let paragraph = document.querySelector("p");
+
+paragraph.innerHTML = "Escolha um número entre 01 e 10";
+
+let btnGuess = document.querySelector('.container__botao')
+
+/*
+btnGuess.addEventListener('click', () => {
+        console.log("Teste")  
+}
+)
+*/
+
+function validateGuess(){
+    console.log('test')
+}
