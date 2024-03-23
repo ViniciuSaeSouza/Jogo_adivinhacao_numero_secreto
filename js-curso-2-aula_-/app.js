9cbb65cbe2a2cbbcc01ed2d472ab2c88042e208f
Vinicius Saes <viniciusaesouza@gmail.com>
2024-03-23 16:05:09 -0300

    Update app.js

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,5 +1,8 @@
 let randomNumber = randomNumberGenerator();
 let btnGuess = document.querySelector(".container__botao");
+let chances = 1;
+
+console.log(randomNumber);
 
 function showTextOnScreen(tag, texto) {
   let campo = document.querySelector(tag);
@@ -9,17 +12,26 @@ function showTextOnScreen(tag, texto) {
 showTextOnScreen("h1", "Jogo do número secreto");
 showTextOnScreen("p", "Escolha um número entre 1 e 100");
 
+function clearText() {
+  guess = document.querySelector("input");
+  guess.value = "";
+}
+
 function validateGuess() {
   let guess = document.querySelector("input").value;
   if (guess == randomNumber) {
     showTextOnScreen("h1", "Acertou!");
-    showTextOnScreen("p", "Você acertou o número secreto!");
+    let trysText = chances > 1 ? "tentativas" : "tentativa";
+    let chancesText = `Você acertou o número secreto com ${chances} ${trysText}`;
+    showTextOnScreen("p", chancesText);
   } else {
     if (guess > randomNumber) {
       showTextOnScreen("p", "O número secreto é menor! ");
     } else {
       showTextOnScreen("p", "O número secreto é maior!");
     }
+    chances++;
+    clearText();
   }
 }
 
